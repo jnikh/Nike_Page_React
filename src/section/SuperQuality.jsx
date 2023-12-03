9c9ebae45d4b18d004594451f82d9b11e4731375
Nikhil <jhanikhil881@gmail.com>
2023-12-03 14:02:22 +0530

    changes

--- src/section/SuperQuality.jsx
+++ src/section/SuperQuality.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const SuperQuality = () => {
   return (
-    <div>
-      
-    </div>
+    <section
+     id="about-us"
+     className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'
+    > 
+     <div className='flex flex-1 flex-col'>
+    
+
+     </div>
+
+    </section>
   )
 }
 
